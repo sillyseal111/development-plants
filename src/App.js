2d5f20369fe6911b809cd4eb2fdc033af7219bf6
indiameicorengold <115094972+sillyseal111@users.noreply.github.com>
2022-11-15 15:25:50 -0500

    general layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
 import './App.css';
-import FilteredList from './FilteredList';
+import { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-  const productList = [
+  const [type, setType] = useState("All");
+
+  const plantList = [
     { name: "Cute Plant", size: "Medium"},
     { name: "Happy Plant", size: "Large"},
     { name: "Sad Plant", size: "Desktop"},
     { name: "Smart Plant", size: "Medium"},
     { name: "Sleepy Plant", size: "Desktop"}
    ]
-   
+
+   function Plant(props) {
+    return (
+      <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{props.name}</Card.Title>
+        <Card.Text>
+          {props.size}
+        </Card.Text>
+        <Button variant="success">Add to cart</Button>
+      </Card.Body>
+      </Card>
+    );
+   }
+
+   function selectFilterType() {
+
+   }
 
   return (
     <div className="App">
-      <h1>Hello</h1>
-      {/* <FilteredList list={productList} /> */}
+      <h1>Plants!</h1>
+      <Nav>
+        <Nav.Item>
+          <Nav.Link eventKey="All" onSelect={selectFilterType}>All</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="Medium" onSelect={selectFilterType}>Medium</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="Large" onSelect={selectFilterType}>Large</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="Desktop" onSelect={selectFilterType}>Desktop</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      
+      {plantList.map((item) => (
+        <Plant name = {item.name}
+        size = {item.size} />
+      ))}
     </div>
   );
 }
